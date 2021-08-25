690c829dfe64af2f9dd309d81dca2005b4590be7
BigManFisherMan <89546641+BigManFisherMan@users.noreply.github.com>
2021-08-25 15:45:36 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/****************************************************************
+* LAB 1 Hello.java
+**************************************************************
+* Nathan DeVore
+* 8/24/21
+* CMSC 255 C90
+***************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("Nathan");
+		System.out.println("Hello.java");
+		System.out.println("Lab 1, Printing stuff, very colorful");
+		System.out.println("8/24/21");
+		System.out.println("CMSC 255, C90");
+		System.out.println();
+	}
+}
